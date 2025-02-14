b1282ae958248636954c3e65a3142ec5e9df0499
SJeongheon <fight0105@gmail.com>
2025-02-14 19:29:51 +0900

    Refactor: Error Page     유저에서 error message 보이지 않게 하기

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,7 +11,7 @@ export default function GlobalError({ error, reset }: { error: Error; reset: () 
         alt="Curious Mouse"
         className={styles.errorImage}
       />
-      <h1 className={styles.title}>{error.name} {error.message}</h1>
+      <h1 className={styles.title}>{error.name}</h1>
       <p className={styles.description}>서버가 아파요. 나중에 다시 시도해주세요..</p>
       <Link href="/" className={styles.backButton}>
         홈으로 돌아가기
