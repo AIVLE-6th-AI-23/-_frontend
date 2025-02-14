1e61a80d1ec4802b60d7219d62a279950af6143d
SJeongheon <fight0105@gmail.com>
2025-02-14 19:34:52 +0900

    Refactor : get queryparam    useSearchParams() should be wrapped in a suspense boundary    강제 CSR로 방식 전환

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { verifyUser, resetPassword } from '@/services/user';
 import { motion } from 'framer-motion';
 import * as styles from './resetPassword.css';
@@ -11,8 +11,12 @@ import { DepartmentOptions } from '@/constants/constants';
 
 export default function ResetPasswordPage() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token');
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setToken(params.get('token'));
+  }, []);
 
   const [step, setStep] = useState(token ? 2 : 1);
   const [employeeId, setEmployeeId] = useState('');
@@ -21,7 +25,7 @@ export default function ResetPasswordPage() {
   const [newPassword, setNewPassword] = useState('');
   const [error, setError] = useState('');
 
-  const { mutate: verifyUserMutate, isPending: isVerifying } = useMutation({
+  const { mutate: verifyUserMutate, isPending: isVerifying, error:verifyError } = useMutation({
     mutationFn: () => verifyUser({ employeeId, deptId: department, email }),
     onSuccess: (isVerified) => {
       if (isVerified) {
@@ -30,14 +34,22 @@ export default function ResetPasswordPage() {
         setError('사용자 정보가 올바르지 않습니다.');
       }
     },
+    onError: () => {
+      throw verifyError;
+    },
+    throwOnError: true
   });
 
-  const { mutate: resetPasswordMutate, isPending: isResetting } = useMutation({
+  const { mutate: resetPasswordMutate, isPending: isResetting, error:resetPasswordError } = useMutation({
     mutationFn: () => resetPassword(token as string, newPassword),
     onSuccess: () => {
       alert('비밀번호가 성공적으로 변경되었습니다. 로그인 페이지로 이동합니다.');
       router.push('/login');
     },
+    onError: () => {
+      alert('비밀번호가 변경에 실패하였습니다.');
+      throw resetPasswordError; 
+    }
   });
 
   const handleVerifySubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -52,85 +64,87 @@ export default function ResetPasswordPage() {
   };
 
   return (
-    <div className={styles.container}>
-      <motion.div 
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-        className={styles.resetBox}
-      >
-        <h1 className={styles.title}>비밀번호 재설정</h1>
+    <Suspense>
+      <div className={styles.container}>
+        <motion.div 
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className={styles.resetBox}
+        >
+          <h1 className={styles.title}>비밀번호 재설정</h1>
 
-        {error && <p className={styles.error}>{error}</p>}
+          {error && <p className={styles.error}>{error}</p>}
 
-        {step === 1 && (
-          <motion.form 
-            initial={{ x: 50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.4 }}
-            onSubmit={handleVerifySubmit}
-          >
-            <input
-              type="text"
-              placeholder="사원 번호"
-              value={employeeId}
-              onChange={(e) => setEmployeeId(e.target.value)}
-              required
-              className={styles.input}
-            />
-            <Select 
-              options={DepartmentOptions} 
-              className={styles.input} 
-              placeholder="부서 선택" 
-              onChange={(selectedOption) => setDepartment(selectedOption?.value || '')}
-              isClearable
-            />
-            <input
-              type="email"
-              placeholder="이메일"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className={styles.input}
-            />
-            <button type="submit" disabled={isVerifying} className={styles.button}>
-              {isVerifying ? '확인 중...' : '확인'}
-            </button>
-          </motion.form>
-        )}
+          {step === 1 && (
+            <motion.form 
+              initial={{ x: 50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.4 }}
+              onSubmit={handleVerifySubmit}
+            >
+              <input
+                type="text"
+                placeholder="사원 번호"
+                value={employeeId}
+                onChange={(e) => setEmployeeId(e.target.value)}
+                required
+                className={styles.input}
+              />
+              <Select 
+                options={DepartmentOptions} 
+                className={styles.input} 
+                placeholder="부서 선택" 
+                onChange={(selectedOption) => setDepartment(selectedOption?.value || '')}
+                isClearable
+              />
+              <input
+                type="email"
+                placeholder="이메일"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className={styles.input}
+              />
+              <button type="submit" disabled={isVerifying} className={styles.button}>
+                {isVerifying ? '확인 중...' : '확인'}
+              </button>
+            </motion.form>
+          )}
 
-        {step === 2 && !token && (
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
-            className={styles.message}
-          >
-            비밀번호 재설정 메일이 전송되었습니다. 이메일을 확인해주세요.
-          </motion.p>
-        )}
+          {step === 2 && !token && (
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className={styles.message}
+            >
+              비밀번호 재설정 메일이 전송되었습니다. 이메일을 확인해주세요.
+            </motion.p>
+          )}
 
-        {step === 2 && token && (
-          <motion.form 
-            initial={{ x: 50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.4 }}
-            onSubmit={handleResetSubmit}
-          >
-            <input
-              type="password"
-              placeholder="새로운 비밀번호"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-              required
-              className={styles.input}
-            />
-            <button type="submit" disabled={isResetting} className={styles.button}>
-              {isResetting ? '비밀번호 변경 중...' : '비밀번호 변경'}
-            </button>
-          </motion.form>
-        )}
-      </motion.div>
-    </div>
+          {step === 2 && token && (
+            <motion.form 
+              initial={{ x: 50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.4 }}
+              onSubmit={handleResetSubmit}
+            >
+              <input
+                type="password"
+                placeholder="새로운 비밀번호"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                required
+                className={styles.input}
+              />
+              <button type="submit" disabled={isResetting} className={styles.button}>
+                {isResetting ? '비밀번호 변경 중...' : '비밀번호 변경'}
+              </button>
+            </motion.form>
+          )}
+        </motion.div>
+      </div>
+    </Suspense>
   );
 }
